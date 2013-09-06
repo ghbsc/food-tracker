c98d006fbdfecbaceddfe5e1b81c2db7170900f7
Rog <rcjproject@yahoo.com>
2013-09-05 23:09:48 -0500

    Initial check-in

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,8 @@
 class VisitorsController < ApplicationController
 
   def new
+#    flash[:notice] = 'Welcome!'
+#    flash[:alert] = 'My birthday is soon.'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 FoodTracker::Application.routes.draw do
+  #get "/pages/*id" => 'pages#show'
   root :to => 'visitors#new'
 end
