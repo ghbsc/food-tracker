c4d34e63398f312e4ac085d82a4a9dcc87f069cc
Rog <rcjproject@yahoo.com>
2013-12-21 15:58:07 -0600

    Cleaned up create and update actions in logs controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -11,12 +11,25 @@ class LogsController < ApplicationController
 
     @log= current_user.logs.build(log_params)
     #@log= Log.new(log_params)
-    if @log.save
-      #redirect_to @log, :notice => "Successfully created survey."
-      #redirect_to edit_log_path(@log) 
-    else
-      render 'new'
+
+    save_tags
+
+    respond_to do |format|
+      if @log.save
+        flash[:success] = "Save successful"
+        format.js   {}
+        format.html { redirect_to edit_log_path(logged_date: params[:log][:logged_date].to_date.strftime('%Y-%m-%d')) }
+        format.json { render json: @log, status: :created, location: @log }
+        
+        #redirect_to @log, :notice => "Successfully created survey."
+        #redirect_to edit_log_path(@log) 
+      else
+        format.html { render action: "new" }  
+        format.json { render json: @log.errors, status: :unprocessable_entity }
+        #render 'new'
+      end
     end
+
   end
   
   def edit
@@ -40,22 +53,8 @@ class LogsController < ApplicationController
     set_params
 
     @log = Log.detail(current_user.id, params[:log][:logged_date].to_date)
-   
-    #Add new tags first to the user before adding it to the log
-    if params[:user] && params[:user][:tags]
-      params[:user][:tags].each do |key, value|
-        if value[:id] == '0'
-          @tag = current_user.tags.build(name: value[:name])
-          if @tag.save && params[:log] && params[:log][:tag_ids] && params[:log][:tag_ids][key] == '0' 
-            params[:log][:tag_ids][key] = @tag.id.to_s
-          end
-        end 
-      end
-    end 
-
-    #existing_tag_ids = params[:log][:tag_ids] - ['0'] 
-
-    @log.tag_ids = params[:log][:tag_ids].values if params[:log] && params[:log][:tag_ids]
+  
+    save_tags
 
     respond_to do |format|
       if @log.update_attributes(log_params)
@@ -66,28 +65,16 @@ class LogsController < ApplicationController
             Tag.find(value[:id].to_i).delete if value[:_destroy] == '1'
           end
         end 
-        
+       
+        flash[:success] = "Save successful"
         format.js   {}
-        format.html { redirect_to edit_log_path(logged_date: params[:log][:logged_date].to_date.strftime('%Y-%m-%d')), notice: 'Save successful'}
+        format.html { redirect_to edit_log_path(logged_date: params[:log][:logged_date].to_date.strftime('%Y-%m-%d')) }
         format.json { render json: @log, status: :created, location: @log }
       else
-        format.html { render action: "edit" }
+        format.html { render action: "edit" }  #render 'edit'
         format.json { render json: @log.errors, status: :unprocessable_entity }
       end
     end   
-
-if false 
-    if @log.update_attributes(log_params)
-      
-      #redirect_to @log, :notice  => "Successfully updated survey."
-      
-      
-      flash[:success] = "Save successful"
-      redirect_to edit_log_path(logged_date: params[:log][:logged_date].to_date.strftime('%Y-%m-%d'))
-    else
-      render 'edit'
-    end
-end
 
   end
   
@@ -106,6 +93,24 @@ end
       if params[:log] && params[:log][:meals_attributes]
         set_meal_params 
       end 
+    end
+
+    def save_tags
+      #Add new tags first to the user before adding it to the log
+      if params[:user] && params[:user][:tags]
+        params[:user][:tags].each do |key, value|
+          if value[:id] == '0'
+            @tag = current_user.tags.build(name: value[:name])
+            if @tag.save && params[:log] && params[:log][:tag_ids] && params[:log][:tag_ids][key] == '0' 
+              params[:log][:tag_ids][key] = @tag.id.to_s
+            end
+          end 
+        end
+      end 
+
+      #existing_tag_ids = params[:log][:tag_ids] - ['0'] 
+
+      @log.tag_ids = params[:log][:tag_ids].values if params[:log] && params[:log][:tag_ids]
     end
 
 end
